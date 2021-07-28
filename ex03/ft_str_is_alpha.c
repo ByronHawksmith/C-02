23b7d6a1d646d3819c2301c4085029297063c56d
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-07-28 20:03:57 +0930

    Complete ex03

---
+++ ex03/ft_str_is_alpha.c
@@ -0,0 +1,38 @@
+int	char_is_numeric(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_str_is_alpha(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!char_is_numeric(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int a = ft_str_is_alpha("Hello");
+	int b = ft_str_is_alpha("Hello!");
+	int c = ft_str_is_alpha("Hello0");
+	int d = ft_str_is_alpha("01234");
+	int e = ft_str_is_alpha("");
+	printf("Hello: %d\n", a);
+	printf("Hello1: %d\n", b);
+	printf("Hello0: %d\n", c);
+	printf("01234: %d\n", d);
+	printf(": %d\n", e);
+}
+*/
