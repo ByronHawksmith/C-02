cd4433af7f855dfa950fa50fc16f0c27c49997d7
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-07-28 21:50:33 +0930

    Complete ex07

---
+++ ex07/ft_strupcase.c
@@ -0,0 +1,38 @@
+int	char_is_lower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+char	*ft_strupcase(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (char_is_lower(str[i]))
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char a[6] = "Hello\0";
+	char b[7] = "Hello!\0";
+	char c[7] = "Hello0\0";
+
+	ft_strupcase(a);
+	ft_strupcase(b);
+	ft_strupcase(c);
+	printf("Hello: %s\n", a);
+	printf("Hello1: %s\n", b);
+	printf("Hello0: %s\n", c);
+}
+*/
