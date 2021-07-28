89bb4b8a1ea52b786fbbf1389277e94e07aa6959
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-07-28 19:52:44 +0930

    Complete ex02

---
+++ ex02/ft_str_is_alpha.c
@@ -0,0 +1,40 @@
+int char_is_lower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return 1;
+	else
+		return 0;
+}
+
+int char_is_upper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	else
+		return 0;
+}
+
+int	ft_str_is_alpha(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!char_is_lower(str[i]) && !char_is_upper(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	int a = ft_str_is_alpha("Hello");
+	int b = ft_str_is_alpha("Hello!");
+	int c = ft_str_is_alpha("Hello0");
+	printf("Hello: %d\n", a);
+	printf("Hello1: %d\n", b);
+	printf("Hello0: %d\n", c);
+}
