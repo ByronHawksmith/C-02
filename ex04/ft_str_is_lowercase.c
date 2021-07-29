388c06e904485db245faf7e565311baa26e16a45
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-07-29 21:34:10 +0930

    Modify test case ex04

--- ex04/ft_str_is_lowercase.c
+++ ex04/ft_str_is_lowercase.c
@@ -21,11 +21,13 @@ int	ft_str_is_lowercase(char *str)
 #include <stdio.h>
 int	main(void)
 {
-	int a = ft_str_is_alpha("Hello");
-	int b = ft_str_is_alpha("Hello!");
-	int c = ft_str_is_alpha("Hello0");
+	int a = ft_str_is_lowercase("Hello");
+	int b = ft_str_is_lowercase("Hello!");
+	int c = ft_str_is_lowercase("Hello0");
+	int d = ft_str_is_lowercase("hello");
 	printf("Hello: %d\n", a);
 	printf("Hello1: %d\n", b);
 	printf("Hello0: %d\n", c);
+	printf("hello: %d\n", d);
 }
 */
